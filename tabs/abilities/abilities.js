0e1e408751d29fc6f57641e8ab71bc0c04238721
Riernar <Riernar@users.noreply.github.com>
2022-08-02 18:17:01 +0200

    Started selection of shapeshift

--- tabs/abilities/abilities.js
+++ tabs/abilities/abilities.js
@@ -1,15 +1,34 @@
+function setSelectedShapeshift(rowID) {
+    getSectionIDs("shapeshifts", (IDs) => {
+        const attributeUpdates = {};
+        for(var i=0; i < IDs.length; i++) {
+            attributeUpdates["repeating_shapeshifts_" + IDs[i] + "_selected"] = "0";
+        }
+        attributeUpdates["repeating_shapeshifts_" + rowID + "_selected"] = "1";
+        console.log("Setting active shapeshift to rowID=" + rowID);
+        console.log("Setting attributes: \n" + JSON.stringify(attributeUpdates));
+        k.setAttrs(attributeUpdates);
+    });
+}
+
 function addShapeshift({ attributes, trigger }) {
     k.getAllAttrs({
         callback: (attributes, sections, casc) => {
-            const newRowID = k.generateRowID("repeating_shapeshifts", sections);
-            console.log("Added repeating_shapeshifts rowId: " + newRowID);
-            const newAttributeName = newRowID + "_shapeshift_name";
-            const newAttributes = {};
-            newAttributes[newAttributeName] = "New shapeshift"
-            k.setAttrs(newAttributes);
-
+            const newRow = k.generateRowID("repeating_shapeshifts", sections);
+            console.log("Added repeating_shapeshifts row: " + newRow);
+            const attributeUpdates = {};
+            attributeUpdates[newRow + "_shapeshift_name"] = "New shapeshift";
+            k.setAttrs(attributeUpdates, null, () => {
+                const [section, rowID, attrName] = k.parseTriggerName(newRow + "_shapeshift_name");
+                setSelectedShapeshift(rowID);
+            });
         }
     });
+}
+
+function selectShapeshift({ attributes, trigger }) {
+    const [section, rowID, attrName] = k.parseTriggerName(trigger.name);
+    setSelectedShapeshift(rowID);
     
 }
 
@@ -17,5 +36,4 @@ function removeShapeshift({ attributes, trigger }) {
     console.log("Removed repeating_shapeshifts rowId: <unknown>");
 }
 
-k.registerFuncs({ addShapeshift });
-k.registerFuncs({ removeShapeshift });
+k.registerFuncs({ addShapeshift, selectShapeshift, removeShapeshift });
